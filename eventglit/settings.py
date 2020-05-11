fef30f03e5048d73d9c397c49c72aa2bca4452cc
King Kenway <kingkenway16@gmail.com>
2020-05-11 19:25:12 +0100

    Modified media url

--- eventglit/settings.py
+++ eventglit/settings.py
@@ -171,7 +171,8 @@ LOGOUT_URL = 'logout'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
